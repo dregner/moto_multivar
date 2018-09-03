f4de35665cdf62c5521a4556a38f780968c1fc2e
Daniel Regner <daniel_regner@hotmail.com>
2018-09-03 17:03:25 -0300

    Modelo Interno
    Modelo interno com controle por realimentação de estados

--- matlab/Questao2/questao2.m
+++ matlab/Questao2/questao2.m
@@ -34,8 +34,8 @@ T = [A^3*B-14.715*A*B, A^2*B-14.715*B, A*B, B];
 d = [5.0625, 13.5, 13.5, 6];
 a = [0 , 0, -14.715, 0];
 kd = d-a;
-K = kd*inv(T);
-eig(A-B*K)
+Kl = kd*inv(T);
+eig(A-B*Kl)
 
 %% calculo de L
 
@@ -50,3 +50,27 @@ kdb = db-ab;
 Kb = kdb*inv(Tb)
 L = Kb'
 eig(A-L*C)
+
+%% Modelo Interno
+
+coef = [0 -.1^2 0]
+M = [zeros(2,1) eye(2); coef];
+N = [0;0;1];
+Am = M;
+Bm = N;
+
+Aa = [A zeros(4,3); -Bm*C Am];
+Ba = [B; zeros(3,1)];
+Ca = [C 0];
+%%
+pd = -1;
+Ka = place(Aa,Ba,[pd pd-0.025 pd-0.05 pd-0.075 pd-0.1 pd-0.125 pd-0.15]);
+
+% Verificacao
+polosMF = eig(Aa-Ba*Ka)
+
+% Matriz de ganho para o estado da planta x 
+K = Ka(:,1:4);
+
+% Matriz de ganho para o estado do modelo interno xm
+Km = Ka(:,5:7);
