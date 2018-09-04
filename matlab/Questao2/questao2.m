0e9517ed2c8a3e5a908b082fc2eecec0e3d9f431
Daniel Regner <daniel_regner@hotmail.com>
2018-09-03 21:47:37 -0300

    Observador
    Implementação .m, mas sem sucesso ao diagrama de blocos

--- matlab/Questao2/questao2.m
+++ matlab/Questao2/questao2.m
@@ -14,19 +14,18 @@ A = [0 1 0 0;
     0 0 -m*g/M 0;
     0 0 0 1;
     0 0 (m+M)*g/(M*L) 0];
-%C = [1 0 0 0]; % saída do sistema
 C = [1 0 0 0]; % saida questao 2
 B = [0; 1/M; 0; -1/(M*L)];
 
 Dsim = zeros(4,1);
-x0 = [0 0 pi/4 0];
+x0 = [0 0 pi/4 0]; % condicao inicial do sistema
+x0obs = [0 0 0 0]; % condicao inicial do observador
 
-%% Controlabilidade e Observabilidade
+
+%% Controlabilidade e Observabilidade Letra B
 
 O = [C; C*A; C*A^2; C*A^3];
 Cont = [B A*B A^2*B A^3*B];
-
-syms s;
 
 %% ganho K
 
@@ -50,21 +49,22 @@ kdb = db-ab;
 Kb = kdb*inv(Tb)
 L = Kb'
 eig(A-L*C)
-
-%% Modelo Interno
-
-coef = [0 -.1^2 0]
-M = [zeros(2,1) eye(2); coef];
-N = [0;0;1];
-Am = M;
+%% Modelo Interno Seguir degrau e rejeita pert. degrau. Letra C
+coef = [1 0];
+M = [0 1; 0 1];
+N = [0;1];
+Am = M
 Bm = N;
+Cm = eye(2);
+Dm = zeros(2,1);
 
-Aa = [A zeros(4,3); -Bm*C Am];
-Ba = [B; zeros(3,1)];
+Aa = [A zeros(4,2); -Bm*C Am];
+Ba = [B; zeros(2,1)];
 Ca = [C 0];
-%%
-pd = -1;
-Ka = place(Aa,Ba,[pd pd-0.025 pd-0.05 pd-0.075 pd-0.1 pd-0.125 pd-0.15]);
+
+
+pd = -1.5;
+Ka = place(Aa,Ba,[pd pd-0.025 pd-0.05 pd-0.075 pd-0.1 pd-0.125]);
 
 % Verificacao
 polosMF = eig(Aa-Ba*Ka)
@@ -73,4 +73,41 @@ polosMF = eig(Aa-Ba*Ka)
 K = Ka(:,1:4);
 
 % Matriz de ganho para o estado do modelo interno xm
-Km = Ka(:,5:7);
+Km = Ka(:,5:6);
+
+% %% Modelo Interno  Seguir degrau, rejeita degra e seno 0.1 rad/s Letra E
+% 
+% coef = [0 -.1^2 0]
+% M = [zeros(2,1) eye(2); coef];
+% N = [0;0;1];
+% Am = M;
+% Bm = N;
+% Cm = eye(3);
+% Dm = zeros(3,1);
+% 
+% 
+% Aa = [A zeros(4,3); -Bm*C Am];
+% Ba = [B; zeros(3,1)];
+% Ca = [C 0];
+% 
+% pd = -.1;
+% Ka = place(Aa,Ba,[pd pd-0.025 pd-0.05 pd-0.075 pd-0.1 pd-0.125 pd-0.15]);
+% 
+% % Verificacao
+% polosMF = eig(Aa-Ba*Ka)
+% 
+% % Matriz de ganho para o estado da planta x 
+% K = Ka(:,1:4);
+% 
+% % Matriz de ganho para o estado do modelo interno xm
+% Km = Ka(:,5:7);
+
+%% Observador
+
+% Polo repetido desejado para o observador
+pobs = 2*pd;
+% Matriz de ganho para posicionar os polos de A-LC em pobs
+H = place(A',C',[pobs pobs-0.025 pobs-0.05 pobs-0.075]);
+L = H';
+
+polosObs = eig(A-L*C)
