e59e905b8ba4c4d0499cf86b62c27327aa587a75
Daniel Regner <daniel_regner@hotmail.com>
2018-09-09 13:33:01 -0300

    Resposta sistema dinamico
    Diagrama de blocos para sistema não linear

--- matlab/Questao2/pendulo_n_liear.m
+++ matlab/Questao2/pendulo_n_liear.m
@@ -1,4 +1,4 @@
-function dx = pendulo_n_liear(v){
+function dx = pendulo_n_liear(v)
 
 M = 1;
 L = 1;
@@ -14,7 +14,7 @@ u = v(6);
 dx1 = x2;
 dx2 = (-m*g*sin(x3)+ m*L*x4^2*cos(x3))/((M+m)-m*(cos(x3))^2) + u/((M+m)-m*(cos(x3))^2);
 dx3 = x4;
-dx4 = (-m*cos(x3)/(m+M))*(M+m)/(m*L*(M+m)-m^2*(cos(x3))^2) + (m*g*sin(x3)+m*L*x4^4*sin(x3))*(M+m)/(m*L*(M+m)-m^2*(cos(x3))^2);
+dx4 = u*(-m*cos(x3)/(m+M))*(M+m)/(m*L*(M+m)-m^2*(cos(x3))^2) + (m*g*sin(x3)+m*L*x4^4*sin(x3))*(M+m)/(m*L*(M+m)-m^2*(cos(x3))^2);
 
-dx = [dx1; dx2; dx2; dx4]
+dx = [dx1; dx2; dx3; dx4];
 end
