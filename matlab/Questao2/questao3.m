cc8fbf47cb5d845f13bfd6e5c14ec1f78955cfb5
Daniel Regner <daniel_regner@hotmail.com>
2018-09-27 16:20:50 -0300

    Q3 Fix
    Erro de modelagem sistema dinamico fixado

--- matlab/Questao2/questao3.m
+++ matlab/Questao2/questao3.m
@@ -17,20 +17,23 @@ x0obs = [0; 0; pi/180*10; 0];
 %% Equações não lineares para cada estado
 % dx1 = f1, dx2 = f2, dx3 = f3, dx4 = f4
 f1 = x2;
-f2 = (-m*g*sin(x3)+ m*L*x4^2*cos(x3))/((M+m)-m*(cos(x3))^2);
+f2 = (u-m*g*sin(x3)*cos(x3)+m*L*x4^2*sin(x3))/(M+m-m*cos(x3)^2);
 f3 = x4;
-f4 = (m*g*sin(x3)+m*L*x2^2*sin(x3))*(M+m)/(m*L*(M+m)-m^2*(cos(x3))^2);
+f4_num = m*g*sin(x3)-((u+m*L*x4^2*sin(x3))*m*cos(x3))/(M+m);
+f4_den = m*L-(m^2*L*cos(x3)^2)/(M+m);
+f4 = f4_num/f4_den;
 f = [f1;f2;f3;f4];
         
 u1 = 0;
-u2 = 1/((M+m)-m*(cos(x3))^2);
+u2 = f2;
 u3 = 0;
-u4 = (-m*cos(x3)/(m+M))*(M+m)/(m*L*(M+m)-m^2*(cos(x3))^2);
+%u4 = -(u*m*cos(x3))/((M+m)*m*L-m^2*L*cos(x3)^2);
+u4 = f4;
 uf = [u1;u2;u3;u4];
 
 
 A = double(subs(jacobian(f,x),[x1 x2 x3 x4],[0 0 0 0])); %nao entendo pq retorna matriz 4x3
-B = double(subs(uf,[x3 x4],[0 0]));
+B = double(subs(jacobian(uf,u),[x3 x4 u],[0 0 0]));
 C = [1 0 0 0];
 D = zeros(2,2);
 Con = ctrb(A,B); %Matriz Controlabilidade
